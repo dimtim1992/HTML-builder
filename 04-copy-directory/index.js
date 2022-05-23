bfc678884a3a4d08763ad01a2d643f82c2b5e0ad
Dmitry Tsimoshchanka <36646929+dimtim1992@users.noreply.github.com>
2022-05-24 01:04:11 +0300

    feat: add fourth task decision

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require('fs');
+const path = require('path');
 
+async function copyDir() {
+  try {
+    await fs.promises.access(path.join(__dirname, 'files-copy'), fs.constants.R_OK | fs.constants.W_OK);
+    await fs.promises.rmdir(path.join(__dirname, 'files-copy'), { recursive: true });
+
+    await fs.promises.mkdir(path.join(__dirname, 'files-copy'));
+    const files = await fs.promises.readdir(path.join(__dirname, 'files'), {withFileTypes: true});
+    files.forEach(async (item) => {
+      await fs.promises.writeFile(path.join(__dirname, 'files-copy', item.name), '', () => {});
+      await fs.promises.copyFile(path.join(__dirname, 'files', item.name), path.join(__dirname, 'files-copy', item.name));
+    });
+  } catch (error) {
+    await fs.promises.mkdir(path.join(__dirname, 'files-copy'));
+    const files = await fs.promises.readdir(path.join(__dirname, 'files'), {withFileTypes: true});
+    files.forEach(async (item) => {
+      await fs.promises.writeFile(path.join(__dirname, 'files-copy', item.name), '', () => {});
+      await fs.promises.copyFile(path.join(__dirname, 'files', item.name), path.join(__dirname, 'files-copy', item.name));
+    });
+  }
+}  
+
+copyDir();
