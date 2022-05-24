2d193ce438cc04e04205b5693df53e6345934e58
Dmitry Tsimoshchanka <36646929+dimtim1992@users.noreply.github.com>
2022-05-24 12:07:54 +0300

    fix: change data filter

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,13 +4,9 @@ const path = require('path');
 async function readFiles() {
   const data = await fs.promises.readdir(path.join(__dirname, 'secret-folder'), {withFileTypes: true});
 
-  data.forEach((item, index) => {
-    if(!item.isFile()) {
-      data.splice(index, 1);
-    }
-  });
+  const dataChecked = data.filter((item) => item.isFile());
 
-  data.forEach(async (item) => {
+  dataChecked.forEach(async (item) => {
     const name = item.name.split('.').splice(0, 1)[0];
     const extName = item.name.split('.').splice(1, 1)[0];
     const stat = await fs.promises.stat(path.join(__dirname, 'secret-folder', item.name));
