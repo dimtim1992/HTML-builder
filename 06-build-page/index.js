db5c4def0e1cc4e8ed515af941e3d0e50ed2025b
Dmitry Tsimoshchanka <36646929+dimtim1992@users.noreply.github.com>
2022-05-24 18:21:59 +0300

    fix: add removal directory

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,44 +3,87 @@ const path = require('path');
 
 
 async function buildPage() {
-  try {
-    await fs.promises.access(path.join(__dirname, 'project-dist'), fs.constants.R_OK | fs.constants.W_OK);
-    await fs.promises.rmdir(path.join(__dirname, 'project-dist'), { recursive: true });
-    await fs.promises.mkdir(path.join(__dirname, 'project-dist'));
+  fs.access(path.join(__dirname, 'project-dist'), fs.constants.R_OK | fs.constants.W_OK, async (err) => {
+    if(err) {
+      await fs.promises.mkdir(path.join(__dirname, 'project-dist'));
 
-    let pattern = await fs.promises.readFile(path.join(__dirname, 'template.html'), 'utf-8', () => {});
-    const componentsData = await fs.promises.readdir(path.join(__dirname, 'components'), {withFileTypes: true});
-    const changedComponentsData = componentsData.filter(item => item.isFile() && item.name.split('.')[1] === 'html');
+      let pattern = await fs.promises.readFile(path.join(__dirname, 'template.html'), 'utf-8', () => {});
+      const componentsData = await fs.promises.readdir(path.join(__dirname, 'components'), {withFileTypes: true});
+      const changedComponentsData = componentsData.filter(item => item.isFile() && item.name.split('.')[1] === 'html');
 
-    for (const item of changedComponentsData) {
-      const name = item.name.split('.')[0];
-      const component = await fs.promises.readFile(path.join(__dirname, 'components', item.name), 'utf-8', () => {});
-      pattern = pattern.replace(`{{${name}}}`, component);
-    }
+      for (const item of changedComponentsData) {
+        const name = item.name.split('.')[0];
+        const component = await fs.promises.readFile(path.join(__dirname, 'components', item.name), 'utf-8', () => {});
+        pattern = pattern.replace(`{{${name}}}`, component);
+      }
     
-    await fs.promises.writeFile(path.join(__dirname, 'project-dist', 'index.html'), pattern, () => {});
+      await fs.promises.writeFile(path.join(__dirname, 'project-dist', 'index.html'), pattern, () => {});
 
-    copyDirectory(path.join(__dirname, 'assets'), path.join(__dirname, 'project-dist', 'assets'));
-    uniteCSS();
+      copyDirectory(path.join(__dirname, 'assets'), path.join(__dirname, 'project-dist', 'assets'));
+      uniteCSS();
+    } else {
+      await deleteDir(path.join(__dirname, 'project-dist'));
 
-  } catch (error) {
-    await fs.promises.mkdir(path.join(__dirname, 'project-dist'));
+      await fs.promises.mkdir(path.join(__dirname, 'project-dist'));
 
-    let pattern = await fs.promises.readFile(path.join(__dirname, 'template.html'), 'utf-8', () => {});
-    const componentsData = await fs.promises.readdir(path.join(__dirname, 'components'), {withFileTypes: true});
-    const changedComponentsData = componentsData.filter(item => item.isFile() && item.name.split('.')[1] === 'html');
+      let pattern = await fs.promises.readFile(path.join(__dirname, 'template.html'), 'utf-8', () => {});
+      const componentsData = await fs.promises.readdir(path.join(__dirname, 'components'), {withFileTypes: true});
+      const changedComponentsData = componentsData.filter(item => item.isFile() && item.name.split('.')[1] === 'html');
 
-    for (const item of changedComponentsData) {
-      const name = item.name.split('.')[0];
-      const component = await fs.promises.readFile(path.join(__dirname, 'components', item.name), 'utf-8', () => {});
-      pattern = pattern.replace(`{{${name}}}`, component);
-    }
+      for (const item of changedComponentsData) {
+        const name = item.name.split('.')[0];
+        const component = await fs.promises.readFile(path.join(__dirname, 'components', item.name), 'utf-8', () => {});
+        pattern = pattern.replace(`{{${name}}}`, component);
+      }
     
-    await fs.promises.writeFile(path.join(__dirname, 'project-dist', 'index.html'), pattern, () => {});
+      await fs.promises.writeFile(path.join(__dirname, 'project-dist', 'index.html'), pattern, () => {});
 
-    copyDirectory(path.join(__dirname, 'assets'), path.join(__dirname, 'project-dist', 'assets'));
-    uniteCSS();
-  }
+      copyDirectory(path.join(__dirname, 'assets'), path.join(__dirname, 'project-dist', 'assets'));
+      uniteCSS();
+    }
+  });
+
+
+
+
+  // try {
+  //   await fs.promises.access(path.join(__dirname, 'project-dist'), fs.constants.R_OK | fs.constants.W_OK);
+  //   await fs.promises.rmdir(path.join(__dirname, 'project-dist'), { recursive: true });
+  //   await fs.promises.mkdir(path.join(__dirname, 'project-dist'));
+
+  //   let pattern = await fs.promises.readFile(path.join(__dirname, 'template.html'), 'utf-8', () => {});
+  //   const componentsData = await fs.promises.readdir(path.join(__dirname, 'components'), {withFileTypes: true});
+  //   const changedComponentsData = componentsData.filter(item => item.isFile() && item.name.split('.')[1] === 'html');
+
+  //   for (const item of changedComponentsData) {
+  //     const name = item.name.split('.')[0];
+  //     const component = await fs.promises.readFile(path.join(__dirname, 'components', item.name), 'utf-8', () => {});
+  //     pattern = pattern.replace(`{{${name}}}`, component);
+  //   }
+    
+  //   await fs.promises.writeFile(path.join(__dirname, 'project-dist', 'index.html'), pattern, () => {});
+
+  //   copyDirectory(path.join(__dirname, 'assets'), path.join(__dirname, 'project-dist', 'assets'));
+  //   uniteCSS();
+
+  // } catch (error) {
+  //   await fs.promises.mkdir(path.join(__dirname, 'project-dist'));
+
+  //   let pattern = await fs.promises.readFile(path.join(__dirname, 'template.html'), 'utf-8', () => {});
+  //   const componentsData = await fs.promises.readdir(path.join(__dirname, 'components'), {withFileTypes: true});
+  //   const changedComponentsData = componentsData.filter(item => item.isFile() && item.name.split('.')[1] === 'html');
+
+  //   for (const item of changedComponentsData) {
+  //     const name = item.name.split('.')[0];
+  //     const component = await fs.promises.readFile(path.join(__dirname, 'components', item.name), 'utf-8', () => {});
+  //     pattern = pattern.replace(`{{${name}}}`, component);
+  //   }
+    
+  //   await fs.promises.writeFile(path.join(__dirname, 'project-dist', 'index.html'), pattern, () => {});
+
+  //   copyDirectory(path.join(__dirname, 'assets'), path.join(__dirname, 'project-dist', 'assets'));
+  //   uniteCSS();
+  // }
 }
 
 
@@ -76,25 +119,32 @@ async function uniteCSS() {
       });
     } 
   });
-
-
-
-  // await fs.promises.mkdir(dist, {
-  //   recursive: true,
-  // });
-  // const data = await fs.promises.readdir(src, {withFileTypes: true});
-
-  // for (const item of data) {
-  //   let curSrc = path.join(src, item.name);
-  //   let curDist = path.join(dist, item.name);
-
-  //   if(item.isDirectory()) {
-  //     await fs.promises.mkdir(curDist, { recursive: true });
-  //     copyDirectory(curSrc, curDist);
-  //   } else if(item.isFile()) { 
-  //     await fs.promises.copyFile(curSrc, curDist);
-  //   }  
-  // }
 }
+
+async function deleteDir(src) {
+  const status = await fs.promises.stat(src);
+
+  if (status.isFile()) {
+    console.log('File');
+    await fs.promises.unlink(src);
+  } else if(status.isDirectory()) {
+    const data = await fs.promises.readdir(src, {withFileTypes: true});
+    if(data.length === 0) {
+      console.log('Empty');
+      await fs.promises.rmdir(src);
+    } else {
+      for (const item of data) {
+        console.log('Filled');  
+        let curSrc = path.join(src, item.name);
+        await deleteDir(curSrc);
+        console.log(data);
+        console.log('Empty');  
+      }
+      await fs.promises.rmdir(src);
+      
+    }
+  }
+}
+
 
 buildPage();
